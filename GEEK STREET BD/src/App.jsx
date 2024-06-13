aded76a39451ba22382a9b7aff9721ad61ab64e6
SQL Srijon <116680350+srijon57@users.noreply.github.com>
2024-06-13 23:21:38 +0600

    Fixed Login/Signup issue.

--- GEEK STREET BD/src/App.jsx
+++ GEEK STREET BD/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-//import { Homepage } from "./pages/Homepage/Homepage";
+import { Homepage } from "./pages/Homepage/Homepage";
 import Navbar from ".//components/Header/Navbar";
 import Loginpage  from "./pages/Login/Loginpage";
 import About from "./pages/About/About";
@@ -10,8 +10,9 @@ function App() {
     return (
       <>
       <Navbar/>
-            <Routes>           
-                <Route path="/login" element={<Loginpage />} />
+            <Routes>
+            <Route path="" element={<Homepage/>} />
+                <Route path="/Login" element={<Loginpage />} />
                 <Route path="/About" element={<About />} />
                 <Route path="/Profile" element={<Profile />} />
             </Routes>
