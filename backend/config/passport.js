544fc6b7beedc6eaedf90fd8d2cfc31e0986a81b
SQL Srijon <116680350+srijon57@users.noreply.github.com>
2024-09-11 00:25:09 +0600

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,11 +1,14 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/userModel.js';
+import { config } from "dotenv";
+
+config();
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ email: profile.emails[0].value });
